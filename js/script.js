d7fdd5408dbe33c14b33cb433da04a78b4e9dbf4
aitorLubuntuPortatil <aitorrubiorosales9@gmail.com>
2026-02-17 13:55:22 +0100

    version 1.1

--- js/script.js
+++
@@ -1,8 +0,0 @@
-function Gmail(){
-    const elementoTemporal = document.createElement("textarea");
-    elementoTemporal.value = "aitorrubiorosales9@gmail.com";
-    document.body.appendChild(elementoTemporal);
-    elementoTemporal.select();
-    document.execCommand("copy");
-    document.body.removeChild(elementoTemporal);
-}
